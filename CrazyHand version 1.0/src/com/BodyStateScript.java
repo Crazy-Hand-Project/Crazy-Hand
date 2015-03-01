23afc92126a384222864ad4b1a29b1165045204a
AndrewBischoff9 <adb6fd@mail.mst.edu>
2015-02-28 22:43:28 -0600

    Cleaned up BodyStateScript slightly
    The bit modification functions are not necessary because the class
    extends Script, which already has those same functions. Doesn't affect
    the outcome at all, just wanted to clean it up.

--- CrazyHand version 1.0/src/com/BodyStateScript.java
+++ CrazyHand version 1.0/src/com/BodyStateScript.java
@@ -69,36 +69,7 @@ class BodyStateScript extends Script {
         this.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
 
     }
-    public static int bit(int num, int bit, int set) {
-    	if(set==1){
-    		return num | (1 << bit);
-    	}
-    	else{
-    		return num & ~(1 << bit);
-    	}
 
-     }
-    public static int getBit(int num, int bit) {
-    	return (num >> bit) & 1;
-
-     }
-    
-    public int setBits(int start, int end){
-    	int tmp = 0, length=end-start;
-    	
-    	for(int i = 0; i <= length; i++){
-    		tmp = bit(tmp,length-i,getBit(data[(i+start)/8],7-(i+start)%8));
-    	}
-    	
-    	
-    	return tmp;
-    }
-    public void saveBits(int start, int end, int val){
-    	int length=end-start;
-    	for(int i = 0; i <= length; i++){
-    		data[(i+start)/8] = bit(data[(i+start)/8], 7-(i+start)%8, getBit(val,length-i));
-    	}
-    }
     
     
     public void updateData(){
