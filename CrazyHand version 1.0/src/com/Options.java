d3a73180367a32c3a62c1fe84b9efbe14c6bfc4c
Tater <noEmail>
2015-02-28 19:37:18 -0800

    Simple edit to .gitignore so github doesn't try to sync player data files >.<

---
+++ CrazyHand version 1.0/src/com/Options.java
@@ -0,0 +1,18 @@
+package com;
+
+public class Options
+{
+	public static boolean advanced = false;
+	
+	
+	public static void loadOptions()
+	{
+		
+	}
+	
+	public static void saveOptions()
+	{
+		
+	}
+	
+}
